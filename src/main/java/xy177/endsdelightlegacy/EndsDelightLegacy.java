389732ca76cb7dba69500ee8dae40af66c14134a
xy177 <67099921+xy177@users.noreply.github.com>
2026-05-19 08:55:38 +0800

    Improved JEI hunting drop text display to prevent English overflow while preserving the original meaning.

--- src/main/java/xy177/endsdelightlegacy/EndsDelightLegacy.java
+++ src/main/java/xy177/endsdelightlegacy/EndsDelightLegacy.java
@@ -36,7 +36,7 @@ public class EndsDelightLegacy
 {
     public static final String MODID = "ends_delight";
     public static final String NAME = "End's Delight Legacy";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final CreativeTabs CREATIVE_TAB = EDCreativeTab.TAB;
 
     private static Logger logger;
